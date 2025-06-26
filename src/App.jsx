ccea5e5297175fc6b27d7b7852d664f9033a3632
SafinRweb <safinr.gg@gmail.com>
2025-06-26 23:14:44 +0600

    SPYLT Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import NavBar from './components/NavBar';
-import HeroSection from './sections/HeroSection';
-import gsap from 'gsap';
-import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
-import MessageSection from './sections/MessageSection';
-import FlavorSection from './sections/FlavorSection';
-import { useGSAP } from '@gsap/react';
-import NutritionSection from './sections/NutritionSection';
-import BenefitSection from './sections/BenefitSection';
-import TestimonialSection from './sections/TestimonialSection';
-import FooterSection from './sections/FooterSection';
+import NavBar from "./components/NavBar";
+import HeroSection from "./sections/HeroSection";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+import MessageSection from "./sections/MessageSection";
+import FlavorSection from "./sections/FlavorSection";
+import { useGSAP } from "@gsap/react";
+import NutritionSection from "./sections/NutritionSection";
+import BenefitSection from "./sections/BenefitSection";
+import TestimonialSection from "./sections/TestimonialSection";
+import FooterSection from "./sections/FooterSection";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -16,9 +16,9 @@ const App = () => {
   useGSAP(() => {
     ScrollSmoother.create({
       smooth: 3,
-      effects: true
+      effects: true,
     });
-  })
+  });
 
   return (
     <main>
@@ -29,10 +29,12 @@ const App = () => {
           <MessageSection />
           <FlavorSection />
           <NutritionSection />
+
           <div>
             <BenefitSection />
             <TestimonialSection />
           </div>
+
           <FooterSection />
         </div>
       </div>
--- src/components/ClipPathTitle.jsx
+++ src/components/ClipPathTitle.jsx
@@ -1,27 +1,28 @@
-
-
-const ClipPathTitle = ({title, color, bg, className, borderColor}) => {
+const ClipPathTitle = ({ title, color, bg, className, borderColor }) => {
     return (
-        <div className='general-title'>
-            <div 
-            style={{
-                clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
-                borderColor: borderColor,
-            }}
-            className={`${className} border-[.5vw] text-nowrap opacity-100`}>
-                <div 
+        <div className="general-title">
+            <div
                 style={{
-                    backgroundColor: bg,
+                    clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+                    borderColor: borderColor,
                 }}
-                className="pb-5 md:px-14 px-4 md:pt-0 pt-3">
-                    <h2
+                className={`${className} border-[.5vw] text-nowrap opacity-0`}
+            >
+                <div
+                    className="pb-5 md:px-14 px-3 md:pt-0 pt-3"
                     style={{
-                        color: color,
+                        backgroundColor: bg,
                     }}
-                    >{title}</h2>
+                >
+                    <h2
+                        style={{
+                            color: color,
+                        }}
+                    >
+                        {title}
+                    </h2>
                 </div>
             </div>
-            
         </div>
     );
 };
